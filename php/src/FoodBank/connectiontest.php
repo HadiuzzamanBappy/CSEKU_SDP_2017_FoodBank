3f1aa974e83be9e6faf6a972454c8958f469e1ab
ruby -v <hbappy79@gmail.com>
2017-11-03 12:19:32 +0600

    php connection change

--- php/src/FoodBank/connectiontest.php
+++ php/src/FoodBank/connectiontest.php
@@ -4,11 +4,6 @@
 // $password="";
 // $database_name="foodbank";
 
-$server_name="localhost";
-$user_name="csekua5_feedme";
-$password="Food%^238490232";
-$database_name="csekua5_feedme";
-
 $conn=mysqli_connect($server_name,$user_name,$password,$database_name);
 
 // if($conn){
