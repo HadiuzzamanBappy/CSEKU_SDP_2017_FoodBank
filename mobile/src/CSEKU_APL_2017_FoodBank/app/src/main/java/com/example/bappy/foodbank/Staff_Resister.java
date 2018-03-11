282467f55514a236c81af19df4fcda296e68e6ab
ruby -v <hbappy79@gmail.com>
2018-03-11 19:25:00 +0600

    apk added,user manual new added,external library for Button added and bug fixes

--- mobile/src/CSEKU_APL_2017_FoodBank/app/src/main/java/com/example/bappy/foodbank/Staff_Resister.java
+++ mobile/src/CSEKU_APL_2017_FoodBank/app/src/main/java/com/example/bappy/foodbank/Staff_Resister.java
@@ -1,20 +1,18 @@
 package com.example.bappy.foodbank;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
